4fd0386d785d7eea969a34ea51a0e0a6381bf2ed
AndrewMedvedev42 <sanjino56@gmail.com>
2021-02-02 19:00:56 +0200

    style change, new js code

--- script.js
+++ script.js
@@ -4,14 +4,26 @@ const jokeBtn = document.getElementById("jokebtn")
 jokeBtn.addEventListener("click", genereteJoke)
 genereteJoke()
 
-function genereteJoke(){
+async function genereteJoke(){
     const config = {
         headers:{
             "Accept":"application/json"
     }
 }
-    fetch("https://icanhazdadjoke.com", config)
-        .then((res) => res.json())
-        .then((data) => {jokeElem.innerHTML = data.joke})
+    const res = await fetch("https://icanhazdadjoke.com", config)
+        
+    const data = await res.json()
+
+    jokeElem.innerHTML = data.joke
 }
 
+// function genereteJoke(){
+//     const config = {
+//         headers:{
+//             "Accept":"application/json"
+//     }
+// }
+//     fetch("https://icanhazdadjoke.com", config)
+//         .then((res) => res.json())
+//         .then((data) => {jokeElem.innerHTML = data.joke})
+// }
